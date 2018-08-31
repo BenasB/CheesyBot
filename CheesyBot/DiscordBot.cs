3107f9fbbf296dc2462be2d03a31711ecc254cd3
Benas Budrys <29711974+BenasB@users.noreply.github.com>
2018-08-31 22:16:52 +0300

    Switched to v2, added add command

--- CheesyBot/DiscordBot.cs
+++ CheesyBot/DiscordBot.cs
@@ -3,6 +3,7 @@ using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
--- CheesyBot/Modules/Cheese.cs
+++ CheesyBot/Modules/Cheese.cs
@@ -3,14 +3,17 @@ using System;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace CheesyBot.Modules
 {
     [Group ("cheese")]
     public class Cheese : ModuleBase<SocketCommandContext>
     {
-        static string[] jokes;
-        static string[] Jokes
+        static readonly string path;
+
+        static List<string> jokes;
+        static List<string> Jokes
         {
             get
             {
@@ -20,37 +23,84 @@ namespace CheesyBot.Modules
             }
         }
 
+        static Cheese()
+        {
+            path = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())) + "/jokes.txt";
+        }
+
         [Command]
         public async Task TellAJokeAsync()
         {
             Random rnd = new Random();
-            int randomIndex = rnd.Next(Jokes.Length);
+            int randomIndex = rnd.Next(Jokes.Count);
             string joke = Jokes[randomIndex];
 
             await ReplyAsync(joke);
         }
 
+        [Command]
+        public async Task TellAJokeAsync(int id)
+        {
+            if (id < 0)
+            {
+                await ReplyAsync("The id can't be negative");
+                return;
+            }
+            else if (id >= Jokes.Count)
+            {
+                await ReplyAsync("Can't find a joke with that id");
+                return;
+            }
+
+            string joke = Jokes[id];
+
+            await ReplyAsync(joke);
+        }
+
+        [Command ("add"), RequireOwner]
+        public async Task AddAJokeAsync(params string[] newJokes)
+        {
+            foreach (string joke in newJokes)
+                Jokes.Add(joke);
+
+            SaveJokes();
+            if (newJokes.Length == 1)
+                await ReplyAsync("Joke added!");
+            else
+                await ReplyAsync("Jokes added!");
+        }
+
         [Command ("list")]
         public async Task ListAllJokesAsync()
         {
-            StringBuilder list = new StringBuilder();
-            for (int i = 0; i < Jokes.Length; i++)
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < Jokes.Count; i++)
             {
-                list.Append(i+1);
-                list.Append(". ");
-                list.Append(Jokes[i]);
+                if (builder.Length + 3 + Jokes[i].Length >= 2000)
+                {
+                    await ReplyAsync(builder.ToString());
+                    builder.Clear();
+                }
 
-                if (i != Jokes.Length - 1)
-                    list.Append('\n');
+                builder.Append(i);
+                builder.Append(". ");
+                builder.Append(Jokes[i]);
+
+                if (i != Jokes.Count - 1)
+                    builder.Append('\n');
             }
 
-            await ReplyAsync(list.ToString());
+            await ReplyAsync(builder.ToString());
         }
 
-        static string[] LoadJokes()
+        static List<string> LoadJokes()
+        {         
+            return new List<string>(File.ReadAllLines(path));
+        }
+
+        static void SaveJokes()
         {
-            string path = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())) + "/jokes.txt";
-            return File.ReadAllLines(path);
+            File.WriteAllLines(path, Jokes);
         }
     }
 }
